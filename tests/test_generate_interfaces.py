6a3bcbd1147b50a67f04360203341bfc24f90b48
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2016-06-08 21:49:01 -0400

    Generate our first interface.

--- tests/test_generate_interfaces.py
+++ tests/test_generate_interfaces.py
@@ -1,6 +1,31 @@
+from django.db import models
 from django.test import TestCase
+
+from rest_framework import serializers
 
 
 class CommandTests(TestCase):
+    def setUp(self):
+
+        class MyModel(models.Model):
+            charfield = models.CharField(max_length=200)
+
+        class MySerializer(serializers.ModelSerializer):
+            class Meta:
+                model = MyModel
+
+        self.MySerializer = MySerializer
+
     def test_true(self):
-        self.assertTrue(True)
+        serializer = self.MySerializer()
+        interface = ['interface %s {' % type(serializer).__name__]
+
+        interface_field_mapping = {
+            serializers.IntegerField: 'number',
+            serializers.CharField: 'string',
+        }
+        for name, field in self.MySerializer().fields.items():
+            mapping = interface_field_mapping.get(type(field), 'any')
+            interface.append('    %s = %s;' % (name, mapping))
+        interface.append('}')
+        print("\n".join(interface))
