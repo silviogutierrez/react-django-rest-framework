b1aa29f3fb4211c3b19e1f05933b0632c064535e
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2016-08-08 21:38:16 -0400

    More experiments.

--- tests/test_generate_interfaces.py
+++ tests/test_generate_interfaces.py
@@ -1,7 +1,29 @@
 from django.db import models
 from django.test import TestCase
 
+from enum import Enum
+
 from rest_framework import serializers
+
+
+def foo(enum):
+    def __iter__(self):
+        for index, (x, y) in enumerate(self.__members__.items()):
+            yield x, y.value
+    enum.__class__.__iter__ = __iter__
+    return enum
+
+def bar(enum):
+    to_create = [(name, enum_value.value[0]) for name, enum_value in enum.__members__.items()]
+
+    wrapped_enum = Enum('Animal', to_create)
+
+    def __iter__(self):
+        for _, enum_value in enum.__members__.items():
+            yield enum_value.value[0], enum_value.value[1]
+
+    wrapped_enum.__class__.__iter__ = __iter__
+    return wrapped_enum
 
 
 class CommandTests(TestCase):
@@ -16,6 +38,27 @@ class CommandTests(TestCase):
 
         self.MySerializer = MySerializer
 
+    def test_iterate_over_enum(self):
+
+        @foo
+        class Color(Enum):
+            red = "My first choice"
+            green = "My second choice"
+            blue = "My third choice"
+
+        class Widget(models.Model):
+            @bar
+            class STATUS(Enum):
+                ACCEPTED = (1, 'Accepted')
+                DENIED = (2 , 'Denied')
+
+            color = models.CharField(choices=STATUS)
+
+        print(list(Widget.STATUS))
+        w = Widget(color=Widget.STATUS.ACCEPTED)
+        print(w.color == Widget.STATUS.ACCEPTED)
+
+    """
     def test_true(self):
         serializer = self.MySerializer()
         interface = ['interface %s {' % type(serializer).__name__]
@@ -29,3 +72,4 @@ class CommandTests(TestCase):
             interface.append('    %s = %s;' % (name, mapping))
         interface.append('}')
         print("\n".join(interface))
+    """
