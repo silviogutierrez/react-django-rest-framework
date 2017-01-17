fd8c42cae5269b4d160049d9b8a3e2932079c3a3
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2017-01-16 20:41:03 -0500

    Setup file.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='react_drf',
+      version='0.1',
+      description='TODO',
+      url='https://github.com/silviogutierrez/react-django-rest-framework',
+      author='Silvio J. Gutierrez',
+      author_email='silviogutierrez@gmail.com',
+      license='MIT',
+      packages=['react_drf'],
+      zip_safe=False)
